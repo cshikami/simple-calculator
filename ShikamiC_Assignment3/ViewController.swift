0b2cbdeb7f2f93ea8af39b8e385e8af3a502ade3
Chris Shikami <cshikami@gmail.com>
2017-08-23 16:43:13 -0500

    Add operation functions and switch statements to select them

--- ShikamiC_Assignment3/ViewController.swift
+++ ShikamiC_Assignment3/ViewController.swift
@@ -8,7 +8,28 @@
 
 import UIKit
 
+class Stack {
+    var stackArray = [String]()
+    
+    func push(stringToPush: String) {
+        self.stackArray.append(stringToPush)
+    }
+    
+    func pop() -> String? {
+        if self.stackArray.last != nil {
+            let stringToReturn = self.stackArray.popLast()
+            return stringToReturn
+        }
+        else {
+            return nil
+        }
+    }
+}
+
 class ViewController: UIViewController {
+    
+    var myStack = Stack()
+    //var firstNumberQueue = Queue()
     
     var typingNumber = false
     var firstNumber = 0
@@ -32,37 +53,87 @@ class ViewController: UIViewController {
     @IBAction func addPressed(_ sender: UIButton) {
         typingNumber = false
         firstNumber = Int(calculatorDisplay.text!)!
+        
+        //add "+" to myStack
+        myStack.push(stringToPush: "+")
     }
+    
+    @IBAction func subtractPressed(_ sender: UIButton) {
+        typingNumber = false
+        firstNumber = Int(calculatorDisplay.text!)!
+        //add "-" to myStack
+        myStack.push(stringToPush: "-")
+    }
+    
+    @IBAction func multiplyPressed(_ sender: UIButton) {
+        typingNumber = false
+        firstNumber = Int(calculatorDisplay.text!)!
+        //add "*" to myStack
+        myStack.push(stringToPush: "*")
+    }
+    
+    @IBAction func dividePressed(_ sender: UIButton) {
+        typingNumber = false
+        firstNumber = Int(calculatorDisplay.text!)!
+        myStack.push(stringToPush: "/")
+    }
+    
+    
+    
     
     @IBAction func equalsPressed(_ sender: UIButton) {
         typingNumber = false
         var sum = 0
+        var difference = 0
+        var product = 1
+        var quotient = 1
+        var mySign = "+"
         
         secondNumber = Int(calculatorDisplay.text!)!
-        sum = firstNumber &+ secondNumber
+        
         //if sum < minValue || sum > maxValue {
-          // calculatorDisplay.text = "NaN"
-            
-        //} else {
+        // calculatorDisplay.text = "NaN"
         
+        
+        
+        //pop last sign from stack
+        mySign = myStack.pop()!
+        
+        //work on implementing either switch statements for different signs or hashtable in a separate struct at the top
+        switch mySign {
+            case "+":
+            sum = firstNumber + secondNumber
             calculatorDisplay.text = "\(sum)"
+        case "-":
+            difference = firstNumber - secondNumber
+            calculatorDisplay.text = "\(difference)"
+        case "*":
+            product = firstNumber * secondNumber
+            calculatorDisplay.text = "\(product)"
+        case "/":
+            quotient = firstNumber / secondNumber
+            calculatorDisplay.text = "\(quotient)"
             
-        //}
-        
+        default:
+            Int(calculatorDisplay.text!)!
+        }
         
     }
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
